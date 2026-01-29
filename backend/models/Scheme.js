646e76d8dabfb733babd5f50824c4af4fa33e4da
fierce-adventurer <tripathiayush589@gmail.com>
2026-01-30 00:44:33 +0530

    feat: created db design in Schema.js for MongoDb schema

---
+++ backend/models/Scheme.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+
+const SchemeSchema = new mongoose.Schema({
+    name: {
+        type:String,
+        required: [true, 'Please add new Schema name'],
+        unique: true,
+        trim: true
+    },
+
+    // for government policies and rules and shemes
+    benefits: {
+        type: {
+            type: String,
+            enum: ['Financial' , 'Subsidy', 'Insurance', 'Service'],
+            default: 'financial'
+        },
+        max_value_inr: {
+            type: Number,
+            default: 0
+        },
+        description: String
+    },
+    // to enlist the docs
+    required_documents: [String],
+
+
+    // for RAG and Embeddings
+    original_pdf_url: String,
+    text_chunks: [{
+        content: String,
+        page: Number,
+        // embedding: [Number]
+    }],
+
+    // filtering metadata and fast search
+    filters: {
+        state: [String],
+        gender: [String],
+        caste: [string]
+    }
+}, {timestamps: String});
+
+module.exports = mongoose.model('Scheme', SchemeSchema);
