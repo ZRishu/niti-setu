fddc6b810772e82c521d07d394110e3637672033
fierce-adventurer <tripathiayush589@gmail.com>
2026-01-31 22:26:02 +0530

    feat: wrote logic for extracting text from pdf splitting it putting it into chunks and saving it into db will 201 response for success and 500 for error

---
+++ backend/controllers/schemeConrtoller.js
@@ -0,0 +1,54 @@
+import fs from 'fs';
+import PdfParse from 'pdf-parse';
+import Scheme from '../models/Scheme.js';
+import { splitTextIntoChunks } from '../utils/aiOrchestrator.js';
+import { error } from 'console';
+import { text } from 'stream/consumers';
+
+// response for ingesting scheme pdf
+export const ingestScheme = async (req, res) => {
+    try{
+        if(!req.file){
+            return res.status(400).json({success: false, error: 'Please upload a PDF file'});
+        }
+
+        const { schemeName , benefits, benefitsValue } = req.body;
+        // extracting text from pdf
+        const dataBuffer = fs.readFileSync(req.file.path);
+        const data = await PdfParse(dataBuffer);
+        const rawText = data.text;
+
+        // prep chunks
+        const docs = await splitTextIntoChunks(rawText);
+
+        const textChunks = docs.map((doc) => ({
+            content : doc.pageContent,
+            page : 1,
+        }));
+
+        //saving into mongodb
+        const scheme = await Scheme.create({
+            name: schemeName,
+            benefits: {
+                type: benefitsType || 'Financial',
+                max_value_inr: benefitsValue || 0,
+                description: `Ingested from ${req.file.originalname}`
+            },
+            original_pdf_url : req.file.path,
+            text_chunks: textChunks
+        });
+
+        fs.unlinkSync(req.file.path); // delete the file after processing
+
+        res.status(201).json({
+            success: true, data: {
+                id: scheme._id,
+                name: scheme.name,
+                chunks_processed: textChunks.length
+            }
+        });
+    } catch (err) {
+        console.error('Error ingesting scheme:', err);
+        res.status(500).json({ success: false, error: error.message });
+    }
+}
