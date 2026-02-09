8517ab992f17fa76c6849626f56231d6df1bd0ed
fierce-adventurer <tripathiayush589@gmail.com>
2026-02-09 23:24:43 +0530

    feat: used gemini ai to extract metadata for filters

--- backend/controllers/schemeController.js
+++ backend/controllers/schemeController.js
@@ -3,7 +3,7 @@ import pdfParse from 'pdf-parse/lib/pdf-parse.js';
 import Scheme from '../models/Scheme.js';
 import { getEmbedding, splitTextIntoChunks } from '../utils/aiOrchestrator.js';
 import { get } from 'http';
-import { count } from 'console';
+import { count, log } from 'console';
 
 // response for ingesting scheme pdf
 export const ingestScheme = async (req, res) => {
@@ -16,7 +16,13 @@ export const ingestScheme = async (req, res) => {
     // extracting text from pdf
     const dataBuffer = fs.readFileSync(req.file.path);
     const data = await pdfParse(dataBuffer);
+    //extracting metadata using gemini
     const rawText = data.text;
+
+    //using gemini to extract metadata and filters
+    console.log(`Extracting MetaData using gemini`);
+    const aiMetadata = await extractSchemeDetails(rawText);
+    console.log(`Extracted:`, aiMetadata);
 
     // prep chunks
     const docs = await splitTextIntoChunks(rawText);
@@ -72,7 +78,7 @@ export const searchSchemes = async (req, res) => {
     }
 
     console.log(`🔍 Searching for: "${query}"`);
-    if (userProfile) console.log("👤 User Profile:", userProfile);
+    if (userProfile) console.log('👤 User Profile:', userProfile);
 
     // Convert User Query -> Vector
     const queryVector = await getEmbedding(query);
@@ -86,21 +92,21 @@ export const searchSchemes = async (req, res) => {
       // State Filter: User's State OR "Pan-India" schemes
       if (userProfile.state) {
         conditions.push({
-          "filters.state": { $in: [userProfile.state, "Pan-India"] }
+          'filters.state': { $in: [userProfile.state, 'Pan-India'] },
         });
       }
 
       // Gender Filter: User's Gender OR "All"
       if (userProfile.gender) {
         conditions.push({
-          "filters.gender": { $in: [userProfile.gender, "All"] }
+          'filters.gender': { $in: [userProfile.gender, 'All'] },
         });
       }
 
       // Caste Filter: User's Caste OR "General" OR "All"
       if (userProfile.caste) {
         conditions.push({
-          "filters.caste": { $in: [userProfile.caste, "General", "All"] }
+          'filters.caste': { $in: [userProfile.caste, 'General', 'All'] },
         });
       }
 
@@ -114,23 +120,23 @@ export const searchSchemes = async (req, res) => {
     const results = await Scheme.aggregate([
       {
         $vectorSearch: {
-          "index": "vector_index",
-          "path": "text_chunks.vector",
-          "queryVector": queryVector,
-          "numCandidates": 50,
-          "limit": 5,
-          "filter": searchFilter 
-        }
+          index: 'vector_index',
+          path: 'text_chunks.vector',
+          queryVector: queryVector,
+          numCandidates: 50,
+          limit: 5,
+          filter: searchFilter,
+        },
       },
       {
         $project: {
-          "_id": 1,
-          "name": 1,
-          "benefits": 1,
-          "score": { "$meta": "vectorSearchScore" },
-          "snippet": { "$arrayElemAt": ["$text_chunks.content", 0] },
-        }
-      }
+          _id: 1,
+          name: 1,
+          benefits: 1,
+          score: { $meta: 'vectorSearchScore' },
+          snippet: { $arrayElemAt: ['$text_chunks.content', 0] },
+        },
+      },
     ]);
 
     res.status(200).json({
@@ -138,7 +144,6 @@ export const searchSchemes = async (req, res) => {
       count: results.length,
       data: results,
     });
-
   } catch (err) {
     console.error('Error searching schemes:', err);
     res.status(500).json({ success: false, error: err.message });
